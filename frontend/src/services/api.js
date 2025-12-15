bdd0291c58ec3aee2e015ac2b74de830749adb9d
SmartEduaplhaا <smartedu.alnaggar@gmail.com>
2025-12-15 23:55:52 +0200

    Prepare for Vercel Deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+// لو إحنا على النت استخدم المسار النسبي، لو على الجهاز استخدم 5000
+const API_URL = window.location.hostname === 'localhost' 
+  ? 'http://localhost:5000/api' 
+  : '/api'; 
 
 const api = axios.create({
   baseURL: API_URL,
